9bfb1a843574dea45a9a69103110567753d77e3c
Peter Clarke <petermclarke@gmail.com>
2013-10-18 19:09:29 +0100

    Start of merging too scripts

--- README.md
+++ README.md
@@ -8,5 +8,8 @@ Joystick from https://github.com/jeromeetienne/virtualjoystick.js
 Requirements
 ============
 WebSocket-for-Python - https://github.com/Lawouach/WebSocket-for-Python
+easy_install ws4py
 
 CherryPy - http://www.cherrypy.org/
+http://download.cherrypy.org/cherrypy/3.2.2/CherryPy-3.2.2.tar.gz
+sudo python setup.py install
--- ws_control.py
+++ ws_control.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import argparse
 import random
 import os
+import time
+import RPIO
+import RPIO.PWM
 
 import cherrypy
 
@@ -9,12 +13,128 @@ from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
 from ws4py.websocket import WebSocket
 from ws4py.messaging import TextMessage
 
+PWM_FREQUENCY = 50    # Hz
+
+PWM_DMA_CHANNEL = 0
+PWM_SUBCYLCLE_TIME_US = 1000/PWM_FREQUENCY * 1000
+PWM_PULSE_INCREMENT_US = 5
+
+PAN_PWM_PIN = 23
+TILT_PWM_PIN = 24
+
+ABSOLUTE_MIN_PULSE_WIDTH_US = 500
+ABSOLUTE_MAX_PULSE_WIDTH_US = 2500
+
+#-------------------------------------------------------------------------------
+class ServoPWM:
+
+    #---------------------------------------------------------------------------
+    def __init__( self, pwmPin, minAnglePulseWidthPair,
+        midAnglePulseWidthPair, maxAnglePulseWidthPair ):
+
+        # Check that the given angles are valid
+        assert( minAnglePulseWidthPair[ 0 ] >= 0 )
+        assert( midAnglePulseWidthPair[ 0 ] > minAnglePulseWidthPair[ 0 ] )
+        assert( midAnglePulseWidthPair[ 0 ] < maxAnglePulseWidthPair[ 0 ] )
+        assert( maxAnglePulseWidthPair[ 0 ] <= 180 )
+
+        self.pwmPin = pwmPin
+        self.minAnglePulseWidthPair = minAnglePulseWidthPair
+        self.midAnglePulseWidthPair = midAnglePulseWidthPair
+        self.maxAnglePulseWidthPair = maxAnglePulseWidthPair
+        self.lastPulseWidthSet = None
+
+    #---------------------------------------------------------------------------
+    def setCommand( self, command ):
+
+        # Work out whether the command is an angle, or a pulse width
+        if command >= ABSOLUTE_MIN_PULSE_WIDTH_US:
+            self.setPulseWidth( command )
+        else:
+            self.setAngle( command )
+
+    #---------------------------------------------------------------------------
+    def setPulseWidth( self, pulseWidth ):
+
+        # Constrain the pulse width
+        if pulseWidth < ABSOLUTE_MIN_PULSE_WIDTH_US:
+            pulseWidth = ABSOLUTE_MIN_PULSE_WIDTH_US
+        if pulseWidth > ABSOLUTE_MAX_PULSE_WIDTH_US:
+            pulseWidth = ABSOLUTE_MAX_PULSE_WIDTH_US
+
+        # Ensure that the pulse width is an integer multiple of the smallest
+        # possible pulse increment
+        pulseIncrementUS = RPIO.PWM.get_pulse_incr_us()
+        numPulsesNeeded = int( pulseWidth/pulseIncrementUS )
+        pulseWidth = numPulsesNeeded * pulseIncrementUS
+
+        if pulseWidth != self.lastPulseWidthSet:
+
+            RPIO.PWM.add_channel_pulse( PWM_DMA_CHANNEL, self.pwmPin, 0, numPulsesNeeded )
+            self.lastPulseWidthSet = pulseWidth
+
+    #---------------------------------------------------------------------------
+    def movePulseIncrement( self, pulseIncrement ):
+
+        if self.lastPulseWidthSet is None:
+            command = self.midAnglePulseWidthPair[ 1 ] + pulseIncrement
+        else:
+            command = self.lastPulseWidthSet + pulseIncrement
+
+        # Stop it going too far for this servo's settings.
+        if command < self.maxAnglePulseWidthPair[ 1 ]:
+            command = self.maxAnglePulseWidthPair[ 1 ]
+        elif command >self.minAnglePulseWidthPair[ 1 ]:
+            command = self.minAnglePulseWidthPair[ 1 ]
+
+        print command
+
+        self.setPulseWidth( command )
+
+    #---------------------------------------------------------------------------
+    def setAngle( self, angle ):
+
+        # Constrain the angle
+        if angle < self.minAnglePulseWidthPair[ 0 ]:
+            angle = self.minAnglePulseWidthPair[ 0 ]
+        if angle > self.maxAnglePulseWidthPair[ 0 ]:
+            angle = self.maxAnglePulseWidthPair[ 0 ]
+
+        # Convert the angle to a pulse width using linear interpolation
+        if angle < self.midAnglePulseWidthPair[ 0 ]:
+
+            angleDiff = self.midAnglePulseWidthPair[ 0 ] - self.minAnglePulseWidthPair[ 0 ]
+            startPulseWidth = self.minAnglePulseWidthPair[ 1 ]
+            pulseWidthDiff = self.midAnglePulseWidthPair[ 1 ] - self.minAnglePulseWidthPair[ 1 ]
+
+            interpolation = float( angle - self.minAnglePulseWidthPair[ 0 ] ) / angleDiff
+
+            pulseWidth = startPulseWidth + interpolation*pulseWidthDiff
+
+        else:
+
+            angleDiff = self.maxAnglePulseWidthPair[ 0 ] - self.midAnglePulseWidthPair[ 0 ]
+            startPulseWidth = self.midAnglePulseWidthPair[ 1 ]
+            pulseWidthDiff = self.maxAnglePulseWidthPair[ 1 ] - self.midAnglePulseWidthPair[ 1 ]
+
+            interpolation = float( angle - self.midAnglePulseWidthPair[ 0 ] ) / angleDiff
+
+            pulseWidth = startPulseWidth + interpolation*pulseWidthDiff
+
+        print "Converted angle {0} to pulse width {1}".format( angle, pulseWidth )
+
+        # Now set the pulse width
+        self.setPulseWidth( pulseWidth )
+
+
 class ChatWebSocketHandler(WebSocket):
     def received_message(self, m):
         cherrypy.engine.publish('websocket-broadcast', m)
         # Process pixel data here...
 
         # Move camera...
+        panServoPWM.movePulseIncrement( 5 )
+        time.sleep( 0.05 )
 
     def closed(self, code, reason="A client left the room without a proper explanation."):
         cherrypy.engine.publish('websocket-broadcast', TextMessage(reason))
@@ -156,3 +276,22 @@ if __name__ == '__main__':
             }
         }
     )
+
+    #-------------------------------------------------------------------------------
+    # Create ServoPWM instances to control the servos
+    panServoPWM = ServoPWM( PAN_PWM_PIN,
+        minAnglePulseWidthPair=( 45.0, 1850 ),
+        midAnglePulseWidthPair=( 90.0, 1100 ),
+        maxAnglePulseWidthPair=( 180.0, 500.0 ) )
+    tiltServoPWM = ServoPWM( TILT_PWM_PIN,
+        minAnglePulseWidthPair=( 45.0, 2100 ),
+        midAnglePulseWidthPair=( 90.0, 1700 ),
+        maxAnglePulseWidthPair=( 135.0, 1100.0 ) )
+
+    # Setup RPIO, and prepare for PWM signals
+    RPIO.setmode( RPIO.BCM )
+
+    RPIO.PWM.setup( pulse_incr_us=PWM_PULSE_INCREMENT_US )
+    RPIO.PWM.init_channel( PWM_DMA_CHANNEL, PWM_SUBCYLCLE_TIME_US )
+
+
