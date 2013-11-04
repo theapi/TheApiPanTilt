01435218fcd1452c1aa1a8d84fd77a0c338eb130
Peter Clarke <petermclarke@gmail.com>
2013-11-04 19:06:19 +0000

    def move()

--- theapipantilt/drivers/cube.py
+++ theapipantilt/drivers/cube.py
@@ -77,7 +77,50 @@ class Simulation:
         self.panAngle = 0
         self.tiltAngle = 0
 
+    def move(self):
+        self.screen.fill((0,32,0))
+
+        # It will hold transformed vertices.
+        t = []
+
+        for v in self.vertices:
+            # Rotate the point around X axis, then around Y axis, and finally around Z axis.
+            #r = v.rotateX(self.angle).rotateY(self.angle).rotateZ(self.angle)
+            r = v.rotateX(self.tiltAngle).rotateY(self.panAngle).rotateZ(self.angle)
+
+            # Transform the point from 3D to 2D
+            p = r.project(self.screen.get_width(), self.screen.get_height(), 256, 4)
+            # Put the point in the list of transformed vertices
+            t.append(p)
+
+        # Calculate the average Z values of each face.
+        avg_z = []
+        i = 0
+        for f in self.faces:
+            z = (t[f[0]].z + t[f[1]].z + t[f[2]].z + t[f[3]].z) / 4.0
+            avg_z.append([i,z])
+            i = i + 1
+
+        # Draw the faces using the Painter's algorithm:
+        # Distant faces are drawn before the closer ones.
+        for tmp in sorted(avg_z,key=itemgetter(1),reverse=True):
+            face_index = tmp[0]
+            f = self.faces[face_index]
+            pointlist = [(t[f[0]].x, t[f[0]].y), (t[f[1]].x, t[f[1]].y),
+                         (t[f[1]].x, t[f[1]].y), (t[f[2]].x, t[f[2]].y),
+                         (t[f[2]].x, t[f[2]].y), (t[f[3]].x, t[f[3]].y),
+                         (t[f[3]].x, t[f[3]].y), (t[f[0]].x, t[f[0]].y)]
+            pygame.draw.polygon(self.screen,self.colors[face_index],pointlist)
+
+
+        #self.angle += 1
+
+        pygame.display.flip()
+
     def run(self):
+
+        self.move()
+
         """ Main Loop """
         while 1:
             for event in pygame.event.get():
@@ -86,44 +129,6 @@ class Simulation:
                     sys.exit()
 
             self.clock.tick(50)
-            self.screen.fill((0,32,0))
-
-            # It will hold transformed vertices.
-            t = []
-
-            for v in self.vertices:
-                # Rotate the point around X axis, then around Y axis, and finally around Z axis.
-                #r = v.rotateX(self.angle).rotateY(self.angle).rotateZ(self.angle)
-                r = v.rotateX(self.tiltAngle).rotateY(self.panAngle).rotateZ(self.angle)
-
-                # Transform the point from 3D to 2D
-                p = r.project(self.screen.get_width(), self.screen.get_height(), 256, 4)
-                # Put the point in the list of transformed vertices
-                t.append(p)
-
-            # Calculate the average Z values of each face.
-            avg_z = []
-            i = 0
-            for f in self.faces:
-                z = (t[f[0]].z + t[f[1]].z + t[f[2]].z + t[f[3]].z) / 4.0
-                avg_z.append([i,z])
-                i = i + 1
-
-            # Draw the faces using the Painter's algorithm:
-            # Distant faces are drawn before the closer ones.
-            for tmp in sorted(avg_z,key=itemgetter(1),reverse=True):
-                face_index = tmp[0]
-                f = self.faces[face_index]
-                pointlist = [(t[f[0]].x, t[f[0]].y), (t[f[1]].x, t[f[1]].y),
-                             (t[f[1]].x, t[f[1]].y), (t[f[2]].x, t[f[2]].y),
-                             (t[f[2]].x, t[f[2]].y), (t[f[3]].x, t[f[3]].y),
-                             (t[f[3]].x, t[f[3]].y), (t[f[0]].x, t[f[0]].y)]
-                pygame.draw.polygon(self.screen,self.colors[face_index],pointlist)
-
-
-            #self.angle += 1
-
-            pygame.display.flip()
 
             command = raw_input( ": " )
             command = command.strip().lower()
@@ -132,10 +137,12 @@ class Simulation:
                 if commandLetter == "p":
                     commandData = int( command[ 1: ] )
                     self.panAngle = int (commandData)
+                    self.move()
 
                 if commandLetter == "t":
                     commandData = int( command[ 1: ] )
                     self.tiltAngle = int (commandData)
+                    self.move()
 
 
 if __name__ == "__main__":
